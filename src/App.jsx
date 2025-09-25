2e79a51ac787cc6f3a9b41254d8e59bb7376ced6
emilio <emilioarielguerrasr@gmail.com>
2025-09-25 20:29:13 -0300

    tarea de lenguajes react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ImageUploader from './ImageUploader';
+
+function App() {
+  return (
+    <div>
+      <h1>Tarea Lenguajes IV - React</h1>
+      <ImageUploader />
+    </div>
+  );
+}
+
+export default App
