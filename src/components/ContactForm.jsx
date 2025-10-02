35b3f9c54ff94eed402c7df074d6b4623d7f7997
Matuu-prog <carrizomateo1503@gmail.com>
2025-10-02 19:27:36 -0300

    Agrego mapa en contactos

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -67,7 +67,20 @@ function ContactForm() {
       </form>
 
       {status && <p style={{ marginTop: "10px" }}>{status}</p>}
+      <div className="mapa">
+        <iframe
+          title="Ubicación"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.251181859281!2d-65.41452752543583!3d-24.786851107754195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc3bba3c3fefd%3A0xe77d2372b09385c9!2sHotel%20Alejandro%20I%2C%20Affiliated%20by%20Meli%C3%A1!5e0!3m2!1ses!2sar!4v1759443587547!5m2!1ses!2sar"
+          width="400"
+          height="300"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        </div>
     </div>
+    
   );
 }
 
--- src/data/servicios.js
+++ src/data/servicios.js
@@ -4,21 +4,21 @@ export const servicios = [
     nombre: "Habitación simple",
     descripcion: "Cama individual",
     precio: 30000,
-    imagen: "/images/habitacion-simple.jpg",
+    imagen: "/images/habitacion-simple.jpeg", 
   },
   {
     id: 2,
     nombre: "Habitación doble",
     descripcion: "Cama king size",
     precio: 35000,
-    imagen: "/images/habitacion-doble.jpg",
+    imagen: "/images/habitacion-doble.jpeg",
   },
   {
     id: 3,
     nombre: "Suite",
     descripcion: "Cama king size, minibar, hidromasaje",
     precio: 50000,
-    imagen: "/images/suite.jpg"
+    imagen: "/images/suite.jpeg"
 
   }
 ];
